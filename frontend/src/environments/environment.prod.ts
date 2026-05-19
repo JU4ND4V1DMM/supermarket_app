23253d45feb51acc93e9e38aa5733d8a51f57008
JU4N D3V <161253541+JU4ND4V1DMM@users.noreply.github.com>
2026-05-19 16:02:46 -0500

    API sobre url del back y comunicación con Front

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: '/api', //local proxy in development, same domain in production
-  apiUrl: 'https://supermarket-app-c1su.onrender.com', //production API URL (other domain form render)
+  apiUrl: 'https://supermarket-app-c1su.onrender.com/api', //production API URL (other domain form render)
 };
